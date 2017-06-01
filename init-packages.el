f1000ee8be76cb6289d0bd4525c10787a153a55a
Cha0res <qipengfei@outlook.com>
2017-06-01 23:26:29 +0800

    first commit

---
+++ init-packages.el
@@ -0,0 +1,73 @@
+(require 'cl)
+
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (package-initialize)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+  )
+
+;; add whatever packages
+(defvar cha0res/packages '(
+                           ;; --- Auto-completion ---
+                           company
+			   ;; --- Better Editor ---
+			   hungry-delete
+			   swiper
+			   counsel
+			   smartparens
+			   ;; --- Major Mode ---
+			   js2-mode
+			   ;; --- Minor Mode ---
+			   nodejs-repl
+			   exec-path-from-shell
+			   popwin
+                           ;; --- Themes ---
+			   solarized-theme
+                           ) "Default packages")
+
+(setq package-selected-packages 'cha0res/packages)
+
+
+(defun cha0res/packages-installed-p ()
+  (loop for pkg in cha0res/packages
+        when (not (package-installed-p pkg)) do (return nil)
+        finally (return t)))
+
+(unless (cha0res/packages-installed-p)
+  (message "%s" "Refreshing package database...")
+  (package-refresh-contents)
+  (dolist (pkg cha0res/packages)
+    (when (not (package-installed-p pkg))
+      (package-install pkg))))
+
+
+;; Find Executable Path on OS X
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+(global-hungry-delete-mode)
+
+(require 'smartparens-config)
+;;(add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
+(smartparens-global-mode t)
+
+;; config for swiper
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
+
+
+;; config js2-mode for js files
+(setq auto-mode-alist
+      (append
+       '(("\\.js\\'" . js2-mode))
+       auto-mode-alist))
+
+(global-company-mode t)
+
+(load-theme 'solarized-dark t)
+
+(require 'popwin)
+(popwin-mode t)
+
+(provide 'init-packages)
---
+++ init.el
@@ -0,0 +1,94 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(add-to-list 'load-path "~/.emacs.d/config/")
+
+(require 'init-packages)
+
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+
+(global-auto-revert-mode t)
+
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(setq inhibit-splash-screen t)
+ 
+(global-linum-mode t)
+
+(abbrev-mode t)
+(define-abbrev-table 'global-abbrev-table '(
+					    ;; signature
+					    ("24qi" "Cha0res.K")
+					    ;; Microsoft
+					    ("24ms" "Microsoft")
+					    ))
+
+(setq initial-frame-alist '((width . 120) (height . 40)))
+
+(defun open-my-init-file()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+(global-set-key (kbd "<f2>") 'open-my-init-file)
+
+(require 'recentf)
+(recentf-mode 1)
+(setq recentf-max-menu-item 25)
+(global-set-key (kbd "<f7>") 'recentf-open-files)
+
+;; auto truncate-lines
+(add-hook 'org-mode-hook (lambda () (setq truncate-lines nil)))
+
+(require 'org)
+(setq org-src-fontify-natively t)
+
+(setq org-support-shift-select t)
+
+(delete-selection-mode 1)
+
+(add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
+
+(global-hl-line-mode t)
+
+
+(setq-default cursor-type 'bar)
+
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+
+(global-set-key (kbd "C-h C-f") 'find-function)
+(global-set-key (kbd "C-h C-v") 'find-variable)
+(global-set-key (kbd "C-h C-k") 'find-function-on-key)
+
+(setq org-agenda-files '("~/Documents/org"))
+(global-set-key (kbd "C-c a") 'org-agenda)
+
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-minimum-prefix-length 2)
+ '(company-tooltip-idle-delay 0.08))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
